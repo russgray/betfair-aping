66bd979dea6bf2578b8e0e23f0cc583a0c1c9b87
Russell Gray <russell.gray@livedrive.com>
2014-12-22 14:38:24 +0000

    Start adding betting api

---
+++ src/BetfairAPING/BettingApi.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace BetfairAPING
+{
+    public class BettingApi : BetfairApiNextGen
+    {
+        public BettingApi(string appKey, string subdomain = null, string sessionToken = null)
+            : base(appKey, subdomain ?? "api", "betting", sessionToken)
+        {
+            
+        }
+
+        public async Task<CompetitionResult> ListCompetitionsAsync(dynamic payload = null)
+        {
+            return await SendRequest<CompetitionResult>("listCompetitions", payload: payload);
+        }
+    }
+
+    public class CompetitionResult
+    {
+    }
+}
