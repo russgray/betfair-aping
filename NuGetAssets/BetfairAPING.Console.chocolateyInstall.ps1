00d268260c46de469a704c861743ed1e4901d555
Russell Gray <russell.gray@livedrive.com>
2015-01-16 16:21:58 +0000

    Use costura to merge assemblies, and package app for chocolatey

---
+++ NuGetAssets/BetfairAPING.Console.chocolateyInstall.ps1
@@ -0,0 +1,2 @@
+Generate-BinFile "bfapi" "$packageFolder\Tools\betfairapi.exe"
+Write-ChocolateySuccess "BetfairAPI.Console"
---
+++ NuGetAssets/BetfairAPING.Console.chocolateyUninstall.ps1
@@ -0,0 +1,3 @@
+Remove-BinFile "bfapi" "$packageFolder\Tools\betfairapi.exe"
+Remove-BinFile "betfairapi" "$packageFolder\Tools\betfairapi.exe"
+Write-ChocolateySuccess "BetfairAPING.Console"
